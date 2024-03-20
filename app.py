43d268ddc275dbfb810a0fa6352d2e22bc39156b
Ivan Dyominov <dema.ivan@gmail.com>
2024-03-20 21:08:41 +0200

    update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from joblib import load
 import pandas as pd
 import logging
 
 app = Flask(__name__)
 
-# Загружаем модели и OneHotEncoder
+# Загрузка моделей и OneHotEncoder
 model_total = load('model_total.joblib')
 model_home = load('model_home.joblib')
 model_away = load('model_away.joblib')
@@ -16,10 +16,8 @@ df = pd.read_csv(file_path)
 
 threshold = 2  # Пороговое значение для сравнения экстремумов
 
-
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -27,74 +25,84 @@ def predict():
     responses = []  # Список для хранения ответов на каждую игру
 
     for game in games_data:
+        if 'awayTeam' not in game or 'homeTeam' not in game:
+            logging.warning("В объекте игры отсутствуют ключи 'awayTeam' или 'homeTeam'")
+            continue
+
         away_team = game['awayTeam']
         home_team = game['homeTeam']
-        if 'total' in game:
+
+        # Проверка наличия ключевых значений в игре
+        if all(k in game for k in ('total', 'home', 'away')):
             current_total = game['total']
-        else:
-            logging.warning("'total' отсутствует в одном из объектов")
-            continue
-        if 'home' in game:
             current_home = game['home']
-        else:
-            # Если ключ 'total' отсутствует, можно задать значение по умолчанию или обработать иначе
-            logging.warning("'total' отсутствует в одном из объектов")
-            continue
-        if 'away' in game:
             current_away = game['away']
-        # Далее ваш код, который обрабатывает current_total
+        else:
+            logging.warning("Отсутствует один из ключей: 'total', 'home', 'away'")
+            continue
 
         filtered_df = df[(df['homeTeam'] == home_team) & (df['awayTeam'] == away_team)]
-    
-        # Вычисляем статистику по фильтрованному DataFrame
-        min_total_score, max_total_score = filtered_df['totalScores'].min(), filtered_df['totalScores'].max()
-        mean_total_score = filtered_df['totalScores'].mean()
-        min_home_score, max_home_score = filtered_df['home'].min(), filtered_df['home'].max()
-        mean_home_score = filtered_df['home'].mean()
-        min_away_score, max_away_score = filtered_df['away'].min(), filtered_df['away'].max()
-        mean_away_score = filtered_df['away'].mean()
-    
+
+        if filtered_df.empty:
+            logging.warning(f"Данные для команд {home_team} и {away_team} не найдены")
+            continue
+
+        # Вычисление статистики по фильтрованному DataFrame
+        stats = calculate_statistics(filtered_df)
+
         teams_for_prediction = pd.DataFrame({'awayTeam': [away_team], 'homeTeam': [home_team]})
         encoded_teams = one_hot_encoder.transform(teams_for_prediction).toarray()
         encoded_teams_df = pd.DataFrame(encoded_teams, columns=one_hot_encoder.get_feature_names_out())
-    
+
         # Делаем предсказания
-        predicted_total = model_total.predict(encoded_teams_df)[0]
-        predicted_home = model_home.predict(encoded_teams_df)[0]
-        predicted_away = model_away.predict(encoded_teams_df)[0]
-    
+        predictions = {
+            'total': model_total.predict(encoded_teams_df)[0],
+            'home': model_home.predict(encoded_teams_df)[0],
+            'away': model_away.predict(encoded_teams_df)[0]
+        }
+
         # Проверяем условия для текущих и предсказанных значений
         messages = []
-        check_extremes_and_predictions(messages, current_total, min_total_score, max_total_score, mean_total_score,
-                                       predicted_total, 'тотал', 12)
-        check_extremes_and_predictions(messages, current_home, min_home_score, max_home_score, mean_home_score,
-                                       predicted_home, 'домашний счет', 9)
-        check_extremes_and_predictions(messages, current_away, min_away_score, max_away_score, mean_away_score,
-                                       predicted_away, 'выездной счет', 9)
-    
+        messages += check_extremes_and_predictions(current_total, stats['total'], predictions['total'], 'total', threshold)
+        messages += check_extremes_and_predictions(current_home, stats['home'], predictions['home'], 'home', threshold)
+        messages += check_extremes_and_predictions(current_away, stats['away'], predictions['away'], 'away', threshold)
+
         # Формируем ответ для текущей игры
         responses.append({'homeTeam': home_team, 'awayTeam': away_team, 'messages': messages})
 
     # Возвращаем список ответов для всех игр
     return jsonify(responses)
 
+def calculate_statistics(filtered_df):
+    return {
+        'total': {
+            'min': filtered_df['totalScores'].min(),
+            'max': filtered_df['totalScores'].max(),
+            'mean': filtered_df['totalScores'].mean()
+        },
+        'home': {
+            'min': filtered_df['home'].min(),
+            'max': filtered_df['home'].max(),
+            'mean': filtered_df['home'].mean()
+        },
+        'away': {
+            'min': filtered_df['away'].min(),
+            'max': filtered_df['away'].max(),
+            'mean': filtered_df['away'].mean()
+        }
+    }
 
-def check_extremes_and_predictions(messages, current_value, min_value, max_value, mean_value, predicted_value, value_name, diff_threshold, threshold):
-    """Функция для проверки текущих и предсказанных значений и указания конкретных отличий."""
-    if abs(current_value - mean_value) > diff_threshold:
-        messages.append(f'Текущий {value_name} ({current_value}) значительно отличается от среднего ({mean_value}).')
-
-    if abs(current_value - min_value) <= threshold:
-        messages.append(f'Текущий {value_name} ({current_value}) очень близок к минимальному значению ({min_value}).')
-
-    if abs(current_value - max_value) <= threshold:
-        messages.append(f'Текущий {value_name} ({current_value}) очень близок к максимальному значению ({max_value}).')
-
-    if abs(current_value - predicted_value) > diff_threshold:
+def check_extremes_and_predictions(current_value, stats, predicted_value, value_name, threshold):
+    messages = []
+    if abs(current_value - stats['mean']) > threshold:
+        messages.append(f'Текущий {value_name} ({current_value}) значительно отличается от среднего ({stats["mean"]}).')
+    if abs(current_value - stats['min']) <= threshold:
+        messages.append(f'Текущий {value_name} ({current_value}) очень близок к минимальному значению ({stats["min"]}).')
+    if abs(current_value - stats['max']) <= threshold:
+        messages.append(f'Текущий {value_name} ({current_value}) очень близок к максимальному значению ({stats["max"]}).')
+    if abs(current_value - predicted_value) > threshold:
         messages.append(f'Текущий {value_name} ({current_value}) сильно отличается от предсказанного значения ({predicted_value}).')
-
-
+    return messages
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
